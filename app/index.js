cf9ce14fb795d998f0883d7447d05f1c8d8961e2
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-06-12 16:42:15 -0500

    updated index.js to reflect from ec2

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello from CI/CD pipeline and GitHub workflow 2 and from RV and more WOOOOOOOOW yeah!'));
+app.get('/', (req, res) => res.send('Hello from CI/CD pipeline, Great learning and grateful for it! Here I come, from EC2!'));
 app.listen(3000, () => console.log('App running on port 3000'));
