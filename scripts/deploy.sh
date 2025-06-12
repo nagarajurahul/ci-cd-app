0f81bc177497ef885e7495ab5374da9e9f8ef08f
Rahul Nagaraju <143856412+nagarajurahul@users.noreply.github.com>
2025-06-12 16:17:28 -0500

    Update deploy.sh - added container port

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,9 @@ echo "📦 Ensuring Docker is installed and running..."
 
 # Check for Docker
 if ! command -v docker &> /dev/null; then
-  echo "🔧 Docker not found. Installing with dnf..."
+  echo "🔧 Docker not found. Installing..."
+  # sudo apt update
+  # sudo apt install -y docker.io
   sudo dnf update -y
   sudo dnf install -y docker
   sudo systemctl enable docker
@@ -22,12 +24,14 @@ else
   echo "✅ Docker is already running."
 fi
 
-echo "Pulling latest Docker image..."
+echo "🪪 Logging into Docker Hub..."
 echo "$DOCKERHUB_TOKEN" | sudo docker login -u "$DOCKERHUB_USERNAME" --password-stdin
 
+echo "⇣ Pulling latest Docker image..."
 sudo docker pull "$DOCKERHUB_USERNAME/$DOCKERHUB_IMAGE:$DOCKERHUB_TAG"
 
-echo "Restarting Docker container..."
+echo "🔄 Restarting Docker container..."
+# sudo docker stop "$DOCKER_CONTAINER_NAME" || true
 sudo docker kill "$DOCKER_CONTAINER_NAME" || true
 sudo docker rm "$DOCKER_CONTAINER_NAME" || true
-sudo docker run -d --name "$DOCKER_CONTAINER_NAME" -p 3000:3000 "$DOCKERHUB_USERNAME/$DOCKERHUB_IMAGE:$DOCKERHUB_TAG"
+sudo docker run -d --name "$DOCKER_CONTAINER_NAME" -p "$DOCKER_CONTAINER_PORT":"$DOCKER_CONTAINER_PORT"  "$DOCKERHUB_USERNAME/$DOCKERHUB_IMAGE:$DOCKERHUB_TAG"
