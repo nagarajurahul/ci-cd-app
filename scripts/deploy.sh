0a89fda39df430d1c61c6bd35c1fa5259675863c
Rahul Nagaraju <143856412+nagarajurahul@users.noreply.github.com>
2025-06-12 15:26:06 -0500

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -23,11 +23,11 @@ else
 fi
 
 echo "Pulling latest Docker image..."
-echo "$DOCKERHUB_TOKEN" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
+echo "$DOCKERHUB_TOKEN" | sudo docker login -u "$DOCKERHUB_USERNAME" --password-stdin
 
-docker pull "$DOCKERHUB_USERNAME"/"$DOCKERHUB_IMAGE":"$DOCKERHUB_TAG"
+sudo docker pull "$DOCKERHUB_USERNAME/$DOCKERHUB_IMAGE:$DOCKERHUB_TAG"
 
 echo "Restarting Docker container..."
-docker kill my-app || true
-docker rm my-app || true
-docker run -d --name "$DOCKER_CONTAINER_NAME" -p 3000:3000 "$DOCKERHUB_USERNAME"/"$DOCKERHUB_IMAGE":"$DOCKERHUB_TAG"
+sudo docker kill my-app || true
+sudo docker rm my-app || true
+sudo docker run -d --name "$DOCKER_CONTAINER_NAME" -p 3000:3000 "$DOCKERHUB_USERNAME/$DOCKERHUB_IMAGE:$DOCKERHUB_TAG"
