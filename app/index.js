b17fb3559d206355948d9e31f9d7543ccd611e04
Rahul Nagaraju <143856412+nagarajurahul@users.noreply.github.com>
2025-07-01 14:43:42 -0500

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello from CI/CD pipeline, Great learning and grateful for it! Here I come, from my K8s server - node2 and argocd and image updater -bupdate 2!'));
+app.get('/', (req, res) => res.send('Hello from CI/CD pipeline, Great learning and grateful for it! Here I come, from my K8s server - node2 and argocd and image updater -bupdate 2 and next update t check!'));
 app.listen(3000, () => console.log('App running on port 3000'));
