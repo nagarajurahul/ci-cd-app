37c71659385195a477c03bebcd9ac22fbbb8158b
Rahul Nagaraju <143856412+nagarajurahul@users.noreply.github.com>
2025-08-17 21:10:35 -0500

    Update index.js

--- app/index.js
+++ app/index.js
@@ -2,4 +2,3 @@ const express = require('express');
 const app = express();
 app.get('/', (req, res) => res.send('Hello from CI/CD pipeline, NodeJS, Docker, GitHub Actions, Helm Charts, ArgoCD, ArgoCD Image Updater, Kubernetes!'));
 app.listen(3000, () => console.log('App running on port 3000'));
-
