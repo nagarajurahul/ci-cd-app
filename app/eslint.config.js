f6e4d238c61729177962b01b45893b3e6168ae55
Rahul Nagaraju <143856412+nagarajurahul@users.noreply.github.com>
2025-09-08 09:04:33 -0500

    update new pluging in eslint for console errors

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -1,7 +1,9 @@
 import js from "@eslint/js";
+import pluginN from "eslint-plugin-n";
 
 export default [
   js.configs.recommended,
+  pluginN.configs["flat/recommended"],
   {
     languageOptions: {
       ecmaVersion: "latest",
@@ -9,7 +11,8 @@ export default [
     },
     rules: {
       semi: ["error", "always"],
-      quotes: ["error", "double"]
+      quotes: ["error", "double"],
+      "n/no-unsupported-features/es-syntax": "off" // we’re using ESM intentionally
     }
   }
 ];
