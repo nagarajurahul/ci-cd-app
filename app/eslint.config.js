618f9f056a684ed0e93ce4f8e502bcb1b5e82c82
Rahul Nagaraju <143856412+nagarajurahul@users.noreply.github.com>
2025-09-07 22:11:43 -0500

    modify eslint.config.js to allow require with commonjs

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -3,6 +3,14 @@ import js from "@eslint/js";
 export default [
   js.configs.recommended,
   {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "commonjs",   // allow require()
+      globals: {
+        console: "readonly",
+        require: "readonly"
+      }
+    },
     rules: {
       semi: ["error", "always"],
       quotes: ["error", "double"]
