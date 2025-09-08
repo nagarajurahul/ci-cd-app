11f87cf5bed59dab2cf576754a93b5f3ff1f8824
Rahul Nagaraju <143856412+nagarajurahul@users.noreply.github.com>
2025-09-07 22:17:26 -0500

    add double quotes instead of single, to solve problems of ESLINT

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-app.get('/', (req, res) => res.send('Hello from CI/CD pipeline, NodeJS, Docker, GitHub Actions, Helm Charts, ArgoCD, ArgoCD Image Updater, Kubernetes!'));
-app.listen(3000, () => console.log('App running on port 3000'));
+app.get("/", (req, res) => res.send("Hello from CI/CD pipeline, NodeJS, Docker, GitHub Actions, Helm Charts, ArgoCD, ArgoCD Image Updater, Kubernetes!"));
+app.listen(3000, () => console.log("App running on port 3000"));
