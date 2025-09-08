9b348c1ee140c5d0727c88ea182d86112e199242
Rahul Nagaraju <143856412+nagarajurahul@users.noreply.github.com>
2025-09-08 08:53:06 -0500

    update index.js with ESM syntax for modern proofing

--- app/index.js
+++ app/index.js
@@ -1,4 +1,14 @@
-const express = require("express");
+// const express = require("express");
+// const app = express();
+// app.get("/", (req, res) => res.send("Hello from CI/CD pipeline, NodeJS, Docker, GitHub Actions, Helm Charts, ArgoCD, ArgoCD Image Updater, Kubernetes!"));
+// app.listen(3000, () => console.log("App running on port 3000"));
+
+import express from "express";
+
 const app = express();
-app.get("/", (req, res) => res.send("Hello from CI/CD pipeline, NodeJS, Docker, GitHub Actions, Helm Charts, ArgoCD, ArgoCD Image Updater, Kubernetes!"));
+app.get("/", (req, res) =>
+  res.send(
+    "Hello from CI/CD pipeline, NodeJS, Docker, GitHub Actions, Helm Charts, ArgoCD, ArgoCD Image Updater, Kubernetes!"
+  )
+);
 app.listen(3000, () => console.log("App running on port 3000"));
